138325e023454d31b1eb0bd19a82036a9178576e
Jonas dos Santos Candeias <jonasscandeias@gmail.com>
2024-07-03 13:28:48 -0300

    Criação 03/07/2024 - Jonas

---
+++ Projeto CRUD/App.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace Projeto_CRUD
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+
+}
