009f87500236cdf3ced9dda3153e7bec7e2feb1a
J6N6S6 <jonasscandeias@gmail.com>
2024-07-06 13:27:01 -0300

    Criação de Connection String

--- Projeto CRUD/App.xaml.cs
+++ Projeto CRUD/App.xaml.cs
@@ -1,6 +1,10 @@
-﻿using System.Configuration;
+﻿using Microsoft.Extensions.Configuration;
+using System;
+using System.IO;
+using System.Configuration;
 using System.Data;
 using System.Windows;
+using System.ComponentModel.DataAnnotations;
 
 namespace Projeto_CRUD
 {
@@ -9,6 +13,30 @@ namespace Projeto_CRUD
     /// </summary>
     public partial class App : Application
     {
+        //Criação da Configuration
+        public static IConfigurationRoot Configuration { get; private set; }
+
+        //Criação do método de inicialização
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+            ConfigureAppSettings();
+            
+            //Com a configuration pronta eu alimento a connection string
+            var connectionString = Configuration.GetConnectionString("DefaultConnection");
+
+            MessageBox.Show(connectionString);
+        }
+
+        //Criação do método que lê o JSON e alimenta a configuration
+        private void ConfigureAppSettings()
+        {
+            var builder = new ConfigurationBuilder().
+                SetBasePath(Directory.GetCurrentDirectory()).
+                AddJsonFile("appsettings.json", optional: false, reloadOnChange: true);
+
+            Configuration = builder.Build();
+        }
     }
 
 }
